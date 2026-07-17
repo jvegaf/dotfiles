688983f4437569fdfb2bab9f53b14f94d80dcc0d
Jose Vega <josevega234@gmail.com>
2026-07-17 13:41:39 +0200

    updates

--- os/linux/polybar/launch.sh
+++ os/linux/polybar/launch.sh
@@ -9,4 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars :D
-polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini topbar &
