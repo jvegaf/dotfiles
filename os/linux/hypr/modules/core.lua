2ba46dcec4f0e6765a436b809f1f68968b1d8fb0
Jose Vega <josevega234@gmail.com>
2026-07-27 20:15:01 +0200

    hypr lua

---
+++ os/linux/hypr/modules/core.lua
@@ -0,0 +1,63 @@
+hl.config({
+	input = {
+		kb_layout = "us",
+		kb_variant = "",
+		kb_model = "",
+		-- kb_options = "grp:alt_shift_toggle,caps:backspace",
+		kb_rules = "",
+		follow_mouse = 1,
+		sensitivity = 0,
+		touchpad = {
+			natural_scroll = true,
+			-- tap_button_map = "lmr",
+		},
+	},
+
+	general = {
+		gaps_in = 2,
+		gaps_out = 0,
+		border_size = 1,
+		col = {
+			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			inactive_border = "rgba(595959aa)",
+		},
+		layout = "dwindle",
+	},
+
+	decoration = {
+		rounding = 4,
+		blur = {
+			enabled = true,
+			passes = 1,
+			new_optimizations = true,
+		},
+		shadow = {
+			enabled = true,
+		},
+	},
+
+	animations = {
+		enabled = false,
+	},
+
+	dwindle = {
+		force_split = 1,
+		preserve_split = true,
+	},
+
+	misc = {
+		disable_hyprland_logo = true,
+		disable_splash_rendering = true,
+		disable_autoreload = false,
+	},
+
+	ecosystem = {
+		no_update_news = true,
+	},
+
+	debug = {
+		disable_logs = false,
+	},
+})
+
+hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
---
+++ os/linux/hypr/modules/env.lua
@@ -0,0 +1,4 @@
+hl.env("HYPRCURSOR_THEME", "rose-pine-hyprcursor")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
